00c8b303d0433a91e93d9c1ba6e3c3b70e7b854f
bet22 <34864818+bet22@users.noreply.github.com>
2018-08-03 15:39:20 +0700

    Чётный массив
    Массив заполняется четными числами

---
+++ ArrayEven.java
@@ -0,0 +1,30 @@
+﻿/**
+* Массив array наполняется случайными числами от 0 до 9
+* Массив arraylist заполняется четными числами из массива array
+* И выводит на экран
+* @autor Betyov A.A.
+* @since 13.01.2018
+* @version 1
+*/
+
+import java.util.ArrayList;
+
+public class ArrayEven {
+
+  public static void main(String[] args) {
+
+    ArrayList<String> arraylist = new ArrayList<String>();
+    int[] array = new int[15];
+
+    for (int i = 0; i < array.length; i++){
+      array[i] = (int) (Math.random() * 9);
+      System.out.print(array[i] + " ");
+      if (array[i] % 2 == 0){
+        arraylist.add(Integer.toString(array[i]));
+      }
+    }	
+
+    System.out.print("\n" + arraylist);
+  }
+
+}
